d454361fb87b0636726f8a74875a942a801c9c50
Emerson Dalwampo <emersondalwampo1120@gmail.com>
2023-03-21 12:33:32 +0800

    change router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import "@fortawesome/fontawesome-free/css/all.min.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import Admin from "./pages/Admin";
 import Faculty from "./pages/Faculty";
 import Home from "./pages/Home";
@@ -10,14 +10,14 @@ export default function App () {
 
   return (
     <>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/admin/*" element={<Admin />} />
           <Route path="/faculty/*" element={<Faculty />} />
           <Route path="/student/*" element={<Student />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </>
   )
 }
